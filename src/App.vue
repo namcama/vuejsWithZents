623b1bb084e49801a2773542511c41f0e4f4dfc7
namcama <nguyen.nhunam.bn@gmail.com>
2022-06-26 11:55:50 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <formlogin/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import formlogin from './components/formlogin.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    formlogin
   }
 }
 </script>
---
+++ src/components/Unit01Ex1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+    export default {
+        name :'Unit01Ex1'
+    }
+</script>
+<style lang="scss">
+    
+</style>
